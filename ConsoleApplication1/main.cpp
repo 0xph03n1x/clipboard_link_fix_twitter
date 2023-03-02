56548f4da9b63378cfab7eca239b45bc4b732fd9
0xph03n1x <lazarovmihail97@gmail.com>
2023-03-02 03:00:12 +0100

    add clipboard link fix functionality

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -9,22 +9,22 @@ void BailOut(const char* msg)
     exit(1);
 }
 
-void NullPointerErr(const char* msg)
-{
-    fprintf(stderr, "Exiting: %s\n", msg);
-    exit(1);
-}
 
 BOOL IsTwitter(std::string clipboard_data)
 {
     if (std::regex_match(clipboard_data, std::regex(("https://twitter.com/.*"))))
     {
-        std::cout << "Twitter link" << std::endl;
+        std::cout << "Twitter link detected. Changing..." << std::endl;
         return true;
+    }
+    else if(std::regex_match(clipboard_data, std::regex(("https://vxtwitter.com/.*"))))
+    {
+        std::cout << "Modified Twitter link detected. No changes needed." << std::endl;
+        return false;
     }
     else
     {
-        std::cout << "Not twitter link" << std::endl;
+        std::cout << "Not a twitter link." << std::endl;
         return false;
     }
 }
@@ -49,24 +49,43 @@ int main()
     printf("Clipboard data: %s\n", (char*)clipboard_data);
 
     char* clipboard_string = static_cast<char*>(GlobalLock(clipboard_data));
+    GlobalUnlock(clipboard_data);
 
     if (clipboard_string == nullptr)
-        NullPointerErr("nullptr error");
+        BailOut("nullptr error");
     
     std::cout << "Clipboard string: " << clipboard_string << std::endl;
 
-    if (clipboard_string != NULL) {
+    if (clipboard_string != NULL) 
+    {
         bool twLink_check = IsTwitter(clipboard_string);
-        if (twLink_check) {
+
+        if (twLink_check)
+        {
             std::string FixedLink = FixTwitterLink(clipboard_string);
             std::cout << "Fixed: " << FixTwitterLink(clipboard_string) << std::endl;
-            /* OpenClipboard() -> Change with SetClipboardData()
-            Figure out how to work with the memory - https://stackoverflow.com/questions/1264137/how-to-copy-string-to-clipboard-in-c
-            
-            SetClipboardData(CF_TEXT, FixedLink);
-            std::cout << "Modified: " << GetClipboardData(CF_TEXT) << std::endl;
-            
-            */
+
+            const char* output = FixedLink.c_str();
+            const size_t link_len = strlen(output) + 1;
+
+            std::cout << "Output value: " << output << std::endl;
+
+
+            HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, link_len);
+
+            if (hMem != NULL)
+            {
+                LPVOID dst = GlobalLock(hMem);
+
+                if (dst != NULL)
+                {
+                    memcpy(dst, output, link_len);
+                    GlobalUnlock(hMem);
+                    OpenClipboard(0);
+                    EmptyClipboard();
+                    SetClipboardData(CF_TEXT, hMem);
+                }
+            }
         }
     }
 
