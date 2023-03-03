8b6611bd0173963ab6bbbef00ce8386244b83c26
0xph03n1x <lazarovmihail97@gmail.com>
2023-03-03 03:10:04 +0100

    window checking functionality

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <regex>
 #include <string>
+#include <fstream>
 
 void BailOut(const char* msg)
 {
@@ -9,15 +10,14 @@ void BailOut(const char* msg)
     exit(1);
 }
 
-
-BOOL IsTwitter(std::string clipboard_data)
+bool IsTwitter(std::string clipboard_data)
 {
     if (std::regex_match(clipboard_data, std::regex(("https://twitter.com/.*"))))
     {
         std::cout << "Twitter link detected. Changing..." << std::endl;
         return true;
     }
-    else if(std::regex_match(clipboard_data, std::regex(("https://vxtwitter.com/.*"))))
+    else if (std::regex_match(clipboard_data, std::regex(("https://vxtwitter.com/.*"))))
     {
         std::cout << "Modified Twitter link detected. No changes needed." << std::endl;
         return false;
@@ -33,26 +33,65 @@ std::string FixTwitterLink(std::string clipboard_string)
 {
     std::string fix = "vx";
     std::string fixed_link = clipboard_string.insert(8, fix);
-    
+
     return fixed_link;
 }
 
+void write_text_to_log_file(const std::string& text)
+{
+    std::ofstream log_file(".\\output.txt", std::ios_base::out | std::ios_base::app);
+    log_file << text << "\n";
+}
+
 // Figure out how to check if foreground window is discord -> check clipboard
-//void CheckWindowName()
-//{
-//    HWND foreground = GetForegroundWindow();
-//    if (foreground)
-//    {
-//        WCHAR window_title[256];
-//        GetWindowText(foreground, window_title, 256);
-//        std::cout << "Window title " << window_title << std::endl;
-//    }
-//
-//}
+std::string CheckWindowName()
+{
+    HWND foreground = GetForegroundWindow();
+    if (foreground)
+    {
+        char window_title[256];
+        GetWindowText(foreground, window_title, 256);
+        std::cout << "Window title: " << window_title << std::endl;
+
+        std::string title_log = "Window title: " + (std::string)window_title;
+        write_text_to_log_file(title_log);
+
+        return window_title;
+
+    }
+
+}
+
+bool DiscordWindow(std::string cWindow)
+{
+
+    if (std::regex_match(cWindow, std::regex(".*Discord")))
+    {
+        std::cout << "Got a match\n" << std::endl;
+        return true;
+    }
+    else
+    {
+        std::cout << "No match\n" << std::endl;
+        return false;
+    }
+}
 
 int main()
 {
-    //CheckWindowName();
+    Sleep(5000);
+    std::string window_title = CheckWindowName();
+    bool res = DiscordWindow(window_title);
+
+    while (res = false)
+    {
+        CheckWindowName();
+        std::cout << window_title << std::endl;
+        if (window_title != window_title)
+            window_title = window_title;
+        res = DiscordWindow(window_title);
+        Sleep(3000);
+    }
 
     HANDLE clipboard_data;
 
